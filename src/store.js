1c6d912dc8176ca7271be98dd2b7a2e34548d739
Max von Tettenborn <contact@max-tettenborn.de>
2021-02-20 19:41:34 +0100

    init meta state on startup

--- src/store.js
+++ src/store.js
@@ -6,11 +6,23 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    count: 0
+    meta: {
+      was_set: false,
+      terminal_id: 'unknown',
+      terminal_name: 'unknown',
+      portal_id: 'unknown'
+    }
   },
   mutations: {
-    increment (state) {
-      state.count++
+    set_meta (state, meta) {
+      if (state.meta.was_set) {
+        throw 'meta has already been set'
+      } else {
+        state.meta.terminal_id = meta.terminal_id;
+        state.meta.terminal_name = meta.terminal_name;
+        state.meta.portal_id = meta.portal_id;
+        state.meta.was_set = true;
+      }
     }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,26 +13,11 @@
         </b-col>
         <b-col sm="3" class="border-left border-top">
           <p>Your Portal:
-            <b-skeleton-wrapper :loading="!portal_id">
-              <template #loading>
-                <b-skeleton width="4em"></b-skeleton>
-              </template>
-              <span v-if="portal_id">{{ portal_id.substring(0, 6) }}</span>
-            </b-skeleton-wrapper>
+            <span>{{ $store.state.meta.portal_id.substring(0, 6) }}</span>
           </p>
           <p>This Terminal:
-            <b-skeleton-wrapper :loading="!terminal_id">
-              <template #loading>
-                <b-skeleton width="4em"></b-skeleton>
-              </template>
-              <span v-if="terminal_id">{{ terminal_id }} </span>
-            </b-skeleton-wrapper>
-            <b-skeleton-wrapper :loading="!terminal_name">
-              <template #loading>
-                <b-skeleton width="4em"></b-skeleton>
-              </template>
-              <span v-if="terminal_name">({{ terminal_name }})</span>
-            </b-skeleton-wrapper>
+            <span>{{ $store.state.meta.terminal_id }} </span>
+            <span>({{ $store.state.meta.terminal_name }})</span>
           </p>
         </b-col>
       </b-row>
@@ -49,34 +34,10 @@ export default {
   components: {AppIcon, navbar},
   data: function () {
     return {
-      portal_id: null,
-      terminal_id: null,
-      terminal_name: null,
     }
   },
-
-  mounted: function () {
-    let component = this;
-    this.$http.get('/core/identity_handler/public/meta/whoami')
-        .then(function (response) {
-
-          if (response.data.type === 'anonymous') {
-            console.log('No token, redirecting to hello world');
-            component.$router.replace('/helloworld');
-
-          } else if (response.data.type === 'terminal') {
-            component.terminal_id = response.data.id;
-            component.terminal_name = response.data.name;
-            component.$http.get('/core/identity_handler/public/meta/whoareyou')
-                .then(function (response) {
-                  component.portal_id = response.data.id;
-                });
-          }
-        });
-  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
