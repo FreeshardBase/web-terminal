68d6a4d576d2d87784e1cc088e7544aae7f05030
Max von Tettenborn <contact@max-tettenborn.de>
2021-03-09 19:19:01 +0100

    add app details modal

--- src/views/Apps.vue
+++ src/views/Apps.vue
@@ -2,12 +2,31 @@
   <div>
     <navbar></navbar>
     <h1>Apps</h1>
+
     <b-table :items="apps" :fields="fields" primary-key="name" hover>
       <template #cell(iconname)="data">
         <img :alt="data.item.name" :src="`/core/app_controller/protected/apps/${data.item.name}/icon`" class="icon">
-        <span class="text-capitalize pl-1">{{ data.item.name }}</span>
+        <a @click="showDetails(data.item)" class="text-capitalize pl-1">{{ data.item.name }}</a>
       </template>
     </b-table>
+
+    <b-modal id="apps-details">
+      <template #modal-header>
+        <span class="text-capitalize">
+          {{ detailItem.name }}
+        </span>
+      </template>
+
+      <template #modal-footer>
+        <b-button variant="outline-danger">Remove</b-button>
+      </template>
+
+      <b-table
+          :items="Object.entries(detailItem)"
+          :fields="[{key: '0', label: 'property'}, {key: '1', label: 'value'}]">
+      </b-table>
+    </b-modal>
+
   </div>
 </template>
 
@@ -23,7 +42,16 @@ name: "Apps",
         {key: 'iconname', sortable: true, label: 'Name'},
         {key: 'version', sortable: true},
         {key: 'installation_reason', sortable: true},
-      ]
+      ],
+
+      detailItem: {},
+    }
+  },
+
+  methods: {
+    showDetails(item) {
+      this.detailItem = item;
+      this.$bvModal.show('apps-details');
     }
   },
 
@@ -44,6 +72,10 @@ name: "Apps",
 
 .icon {
   height: 1.5em;
+}
+
+a {
+  cursor: pointer;
 }
 
 </style>
