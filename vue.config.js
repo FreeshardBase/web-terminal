94c7daba6dca149119dafb921584119d5af04b28
Max von Tettenborn <contact@max-tettenborn.de>
2021-03-21 12:37:45 +0100

    styled hello world view

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/core': {
-        target: 'https://jgqct8.p.getportal.org',
+        target: 'https://lbdlja.p.getportal.org',
         changeOrigin: true,
         cookieDomainRewrite: {
         "*": "localhost",
