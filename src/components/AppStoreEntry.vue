5f4f2448c3d227d2cf4ee6d7bfc16108cf509e2d
Max von Tettenborn <contact@max-tettenborn.de>
2021-04-09 18:20:21 +0200

    button for app installation

--- src/components/AppStoreEntry.vue
+++ src/components/AppStoreEntry.vue
@@ -1,15 +1,18 @@
 <template>
   <b-card class="overflow-hidden" no-body style="max-width: 540px;">
     <b-row no-gutters>
-      <b-col md="2">
-        <b-card-img
-            alt="Icon"
+      <b-col md="2" class="text-center">
+        <b-img
             :src="`/core/app_controller/protected/apps/${app.name}/icon`"
-            class="app-icon m-2"></b-card-img>
+            alt="Icon"
+            class="app-icon m-2"></b-img>
+        <b-button class="m-1" variant="outline-success" @click="installApp">
+          Install
+        </b-button>
       </b-col>
       <b-col md="10">
         <b-card-body :title="app.name">
-          <b-card-text>{{app.description}}</b-card-text>
+          <b-card-text>{{ app.description }}</b-card-text>
         </b-card-body>
       </b-col>
     </b-row>
@@ -20,6 +23,18 @@
 export default {
   name: "AppStoreEntry",
   props: ['app'],
+
+  methods: {
+    installApp() {
+      this.$http.post(`/core/app_controller/protected/store/apps/${this.app.name}`)
+          .then(function (response) {
+            console.log(response)
+          })
+          .catch(function (response) {
+            console.log(response)
+          })
+    }
+  }
 }
 </script>
 
