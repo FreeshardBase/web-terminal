89a1deea7c035c7316ae4f3969618f36fa340e2a
Max von Tettenborn <max@vtettenborn.net>
2022-03-28 17:46:11 +0200

    better layout for app store

--- src/components/AppStoreEntry.vue
+++ src/components/AppStoreEntry.vue
@@ -1,25 +1,60 @@
 <template>
-  <b-card class="overflow-hidden" no-body style="max-width: 540px;">
-    <b-row no-gutters>
-      <b-col md="2" class="text-center">
-        <b-img
-            :src="`/core/protected/apps/${app.name}/icon`"
-            alt="Icon"
-            class="app-icon m-2"></b-img>
+  <div>
+    <b-card class="overflow-hidden" @click="showDetails()" no-body>
+      <b-row no-gutters>
+        <b-col md="2" class="text-center">
+          <b-img
+              :src="`/core/protected/apps/${app.name}/icon`"
+              alt="Icon"
+              class="app-icon m-2"></b-img>
+          <b-badge v-if="app.is_installed" size="xs" pill variant="success">Installed</b-badge>
+        </b-col>
+        <b-col md="10">
+          <b-card-body>
+            <b-card-title>
+              {{ app.name | titlecase }}
+            </b-card-title>
+            <b-card-text>{{ app.description }}</b-card-text>
+          </b-card-body>
+        </b-col>
+      </b-row>
+    </b-card>
+
+    <!-- Modal: app details -->
+    <b-modal :id="`app-details-${ app.name }`">
+      <template #modal-header>
+        <b-container>
+          <b-row align-v="center" align-h="start">
+            <b-col sm="auto" md="auto" lg="auto" xl="auto">
+              <b-img
+                :src="`/core/protected/apps/${app.name}/icon`"
+                alt="Icon"
+                class="app-icon m-2"></b-img>
+            </b-col>
+            <b-col>
+              <h2>
+                {{ app.name | titlecase }}
+              </h2>
+            </b-col>
+          </b-row>
+        </b-container>
+      </template>
+        <div v-if="app.long_description">
+          <p v-for="(paragraph, index) in app.long_description" :key="index">
+            {{ paragraph }}
+          </p>
+        </div>
+        <p v-else>{{ app.description }}</p>
+      <template #modal-footer>
         <b-button v-if="app.is_installed" class="m-1" variant="outline-secondary" disabled>
           Installed
         </b-button>
         <b-button v-else class="m-1" variant="outline-success" @click="installApp">
           Install
         </b-button>
-      </b-col>
-      <b-col md="10">
-        <b-card-body :title="app.name">
-          <b-card-text>{{ app.description }}</b-card-text>
-        </b-card-body>
-      </b-col>
-    </b-row>
-  </b-card>
+      </template>
+    </b-modal>
+  </div>
 </template>
 
 <script>
@@ -37,7 +72,10 @@ export default {
           .catch(function (response) {
             console.log(response)
           })
-    }
+    },
+    showDetails() {
+      this.$bvModal.show(`app-details-${this.app.name}`);
+    },
   }
 }
 </script>
@@ -47,6 +85,17 @@ export default {
 .app-icon {
   height: 4em;
   width: 4em;
+}
+
+.card {
+  max-width: 540px;
+  height: 8em;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.card:hover {
+  box-shadow: 0 0 0.6em -0.3em black;
 }
 
 </style>
